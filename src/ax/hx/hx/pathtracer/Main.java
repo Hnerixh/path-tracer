d8a5ea5e1d53ebad300d00ef0df9be2d039143c2
hnerixh <hx@hx.ax>
2014-04-04 12:41:04 +0200

    add a simple main function

--- src/ax/hx/hx/pathtracer/Main.java
+++ src/ax/hx/hx/pathtracer/Main.java
@@ -1,10 +1,16 @@
 package ax.hx.hx.pathtracer;
 
+import java.io.File;
+
 /**
- * Created by hx on 3/30/14.
+ * This is the simple main file.
  */
 public class Main {
     public static void main(String[] args){
-
+        if (args.length < 2){
+            System.out.println("Please supply at least two arguments.");
+        }
+        Renderer renderer = SceneParser.parseScene(new File(args[0]), new File(args[1]));
+        renderer.render();
     }
 }
